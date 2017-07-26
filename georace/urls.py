283a3a99c6e696ebf418c398efe4c19e333d9645
vomaxHELLnO <spurguoliukas@gmail.com>
2017-07-26 17:52:07 +0300

    add index page hello world!

--- georace/urls.py
+++ georace/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from georace import views
+
 
 urlpatterns = [
+    url(r'^$', views.index),
+
     url(r'^admin/', admin.site.urls),
+
 ]
